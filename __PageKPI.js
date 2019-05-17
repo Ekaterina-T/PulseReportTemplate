2c62c7f93a3463fb0a6ca2ced676e5bc623fd0b3
EkaterinaT-CIT <EkaterinaT-CIT@users.noreply.github.com>
2019-05-17 23:59:42 +0300

    Update __PageKPI.js
    allow to show categories along with questions on page KPI for kpi tiles and trends

--- __PageKPI.js
+++ __PageKPI.js
@@ -1,4 +1,4 @@
-class PageKPI_disabledForDevelopment {
+class PageKPI {
 
     /**
      * @memberof PageKPI
@@ -39,42 +39,61 @@ class PageKPI_disabledForDevelopment {
      * @description function to render the KPI table
      * @param {Object} context - {table: table, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log, suppressSettings: suppressSettings}
      */
-    static function tableKPI_Render(context){
-
-        var report = context.report;
-        var state = context.state;
-        var table = context.table;
-        var log = context.log;
-        var suppressSettings = context.suppressSettings;
-        var pageId = PageUtil.getCurrentPageIdInConfig(context);
-
-        // add row = KPI question
-        var Qs = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPI');
-
-        if (Qs.length == 0) {
-            throw new Error('PageKPI.tableKPI_Render: KPI questions are not specified.');
-        }
-
-        for (var i=0; i < Qs.length; i++) {
-            var qe: QuestionnaireElement = QuestionUtil.getQuestionnaireElement(context, Qs[i]);
-            var row : HeaderQuestion = new HeaderQuestion(qe);
-            row.IsCollapsed = true;
-            row.HideHeader = true;
-            TableUtil.maskOutNA(context, row);
-            table.RowHeaders.Add(row);
-        }
-
-        // add column statics
-        var s : HeaderStatistics = new HeaderStatistics();
-        s.Statistics.Avg = true;
-        table.ColumnHeaders.Add(s);
-
-        // global table settings
-        table.Caching.Enabled = false;
-        //table.RemoveEmptyHeaders.Columns = true;
-        SuppressUtil.setTableSuppress(table, suppressSettings);
-
+  static function tableKPI_Render(context){
+    
+    var report = context.report;
+    var state = context.state;
+    var table = context.table;
+    var log = context.log;
+    var suppressSettings = context.suppressSettings;
+    var pageId = PageUtil.getCurrentPageIdInConfig(context);
+    
+    // add row = KPI question
+    var Qs = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPI');
+    
+    if (Qs.length == 0) {
+      throw new Error('PageKPI.tableKPI_Render: KPI questions are not specified.');
     }
+    
+    for (var i=0; i < Qs.length; i++) {
+      
+      var header = TableUtil.getHeaderDescriptorObject(context, Qs[i]);
+      var row;
+      
+      if(header.Type === 'Question') {        
+        var qe: QuestionnaireElement = QuestionUtil.getQuestionnaireElement(context, header.Code);
+        row = new HeaderQuestion(qe);
+        row.IsCollapsed = true;
+        row.HideHeader = true;
+        
+      } else if(header.Type === 'Dimension') {
+        
+        row = new HeaderCategorization();        
+        row.CategorizationId = String(header.Code).replace(/[ ,&]/g, '');
+        row.DataSourceNodeId = DataSourceUtil.getDsId(context);
+        row.DefaultStatistic = StatisticsType.Average;
+        row.CalculationRule = CategorizationType.AverageOfAggregates; // AvgOfIndividual affects performance
+        row.Preaggregation = PreaggregationType.Average;
+        row.SampleRule = SampleEvaluationRule.Max;// https://jiraosl.firmglobal.com/browse/TQA-4116
+        row.Collapsed = true;
+        row.Totals = true;
+      }      
+      
+      TableUtil.maskOutNA(context, row);
+      table.RowHeaders.Add(row);
+    }
+    
+    // add column statics
+    var s : HeaderStatistics = new HeaderStatistics();
+    s.Statistics.Avg = true;
+    table.ColumnHeaders.Add(s);
+    
+    // global table settings
+    table.Caching.Enabled = false;
+    //table.RemoveEmptyHeaders.Columns = true;
+    SuppressUtil.setTableSuppress(table, suppressSettings);
+    
+  }
 
 
     /**
@@ -111,31 +130,31 @@ class PageKPI_disabledForDevelopment {
      * @description function to render the Trend table
      * @param {Object} context - {table: table, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log, suppressSettings: suppressSettings}
      */
-    static function tableTrend_Render(context){
-
-        var report = context.report;
-        var state = context.state;
-        var table = context.table;
-        var log = context.log;
-        var suppressSettings = context.suppressSettings;
-        var pageId = PageUtil.getCurrentPageIdInConfig(context);
-
-        // add row = KPI question
-        var Qs = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPI');
-
-        for (var i=0; i<Qs.length; i++) {
-            table.RowHeaders.Add(TableUtil.getTrendQuestionHeader(context, Qs[i]));
-        }
-        // add column - trending by Date variable
-        var dateQId = DataSourceUtil.getSurveyPropertyValueFromConfig (context, 'DateQuestion');
-        TableUtil.addTrending(context, dateQId);
-
-        // global table settings
-        table.Caching.Enabled = false;
-        table.Decimals = Config.Decimal;
-        SuppressUtil.setTableSuppress(table, suppressSettings);
-
+  static function tableTrend_Render(context){
+    
+    var report = context.report;
+    var state = context.state;
+    var table = context.table;
+    var log = context.log;
+    var suppressSettings = context.suppressSettings;
+    var pageId = PageUtil.getCurrentPageIdInConfig(context);
+    
+    // add row = KPI question
+    var Qs = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPI');
+    
+    for (var i=0; i<Qs.length; i++) {     
+      table.RowHeaders.Add(TableUtil.getTrendHeader(context, TableUtil.getHeaderDescriptorObject(context, Qs[i])));
     }
+    // add column - trending by Date variable
+    var dateQId = DataSourceUtil.getSurveyPropertyValueFromConfig (context, 'DateQuestion');
+    TableUtil.addTrending(context, dateQId);
+    
+    // global table settings
+    table.Caching.Enabled = false;
+    table.Decimals = Config.Decimal;
+    SuppressUtil.setTableSuppress(table, suppressSettings);
+    
+  }
 
     /**
      * @memberof PageKPI
@@ -144,28 +163,34 @@ class PageKPI_disabledForDevelopment {
      * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
      * @returns {Boolean}
      */
-    static function verbatim_Hide(context){
-
-        if (SuppressUtil.isGloballyHidden(context)) {
-            return true;
-        }
-
-        var log = context.log;
-        var report = context.report;
-
-        // check base value for the verbatim question. If it is less than VerbatimSuppressValue, Verbatim table is hidden
-
-        var counts : Datapoint[] = report.TableUtils.GetColumnValues("VerbatimBase", 1);
-
-        for (var i=0; i<counts.Length; i++) {
-            var base = parseInt(counts[i].Value);
-            if (base < Config.SuppressSettings.VerbatimSuppressValue) {
-                return true;
-            }
-        }
-
-        return false;
+  static function verbatim_Hide(context){
+    
+    if (SuppressUtil.isGloballyHidden(context)) {
+      return true;
     }
+    
+    var log = context.log;
+    var report = context.report;
+    var pageId = PageUtil.getCurrentPageIdInConfig(context);
+    
+    // check if question defined
+    if(!DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPIComment')) {
+      throw new Error('PageKPI.verbatim_Hide: KPI Comment question is not specified.');
+    }
+    
+    // check base value for the verbatim question. If it is less than VerbatimSuppressValue, Verbatim table is hidden
+    
+    var counts : Datapoint[] = report.TableUtils.GetColumnValues("VerbatimBase", 1);
+    
+    for (var i=0; i<counts.Length; i++) {
+      var base = parseInt(counts[i].Value);
+      if (base < Config.SuppressSettings.VerbatimSuppressValue) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
 
     static function verbatim_Render(context) {
 
@@ -191,36 +216,41 @@ class PageKPI_disabledForDevelopment {
      * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
      * @returns {Object} - object with properties title and score
      */
-    static function getKPIResult(context) {
-
-        var report = context.report;
-        var state = context.state;
-        var log = context.log;
-        var pageId = PageUtil.getCurrentPageIdInConfig(context);
-
-        var Qs = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPI');
-        var results = [];
-        for (var i=0; i < Qs.length; i++) {
-            var result = {qid: Qs[i], title: QuestionUtil.getQuestionTitle (context, Qs[i]), score: 'N/A', color: Config.primaryGreyColor};
-
-            if (!SuppressUtil.isGloballyHidden(context) && report.TableUtils.GetRowValues("KPI:KPI",i+1).length) {
-                var cell : Datapoint = report.TableUtils.GetCellValue("KPI:KPI",i+1,1);
-                if (!cell.IsEmpty && !cell.Value.Equals(Double.NaN)) {
-                    result.score = parseFloat(cell.Value.toFixed(Config.Decimal));
-                    var thresholds = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPIthreshold');
-                    for (var j=0; j<thresholds.length; j++) {
-                        if (result.score >= thresholds[j].score) {
-                            result.color =  thresholds[j].color;
-                            break;
-                        }
-                    }
-                }
+  static function getKPIResult(context) {
+    
+    var report = context.report;
+    var state = context.state;
+    var log = context.log;
+    var pageId = PageUtil.getCurrentPageIdInConfig(context);
+    
+    // add row = KPI question
+    var Qs = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPI');    
+    var titles = report.TableUtils.GetRowHeaderCategoryTitles('KPI:KPI');
+    
+    var results = [];
+    for (var i=0; i < Qs.length; i++) {
+      
+      var header = TableUtil.getHeaderDescriptorObject(context, Qs[i]);         
+      var result = {qid: header.Code, title: titles[i], score: 'N/A', color: Config.primaryGreyColor};
+      
+      if (!SuppressUtil.isGloballyHidden(context) && report.TableUtils.GetRowValues("KPI:KPI",i+1).length) {
+        var cell : Datapoint = report.TableUtils.GetCellValue("KPI:KPI",i+1,1);
+        if (!cell.IsEmpty && !cell.Value.Equals(Double.NaN)) {
+          result.score = parseFloat(cell.Value.toFixed(Config.Decimal));
+          var thresholds = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'KPIthreshold');
+          for (var j=0; j<thresholds.length; j++) {
+            if (result.score >= thresholds[j].score) {
+              result.color =  thresholds[j].color;
+              break;
             }
-            results.push(result);
+          }
         }
-
-        return results;
+      }
+      results.push(result);
     }
+   
+      return results;
+  }
 
     /**
      * @memberof PageKPI
