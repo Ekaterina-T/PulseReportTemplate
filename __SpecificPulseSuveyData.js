2362f66e32a13af28ec64ad76dd5bc1fbeab9209
EkaterinaT <Murzik17>
2019-08-22 18:20:57 +0300

    test

---
+++ __SpecificPulseSuveyData.js
@@ -0,0 +1,11 @@
+class __SpecificPulseSuveyData {
+
+    static var resourcesDependentOnSpecificSurvey = {
+        Survey: ['FiltersFromSurveyData'],
+        Page_KPI: ['KPI', 'KPIComment', 'KPIQuestionsToFilterVerbatim'],
+        Page_Trends: ['TrendQuestions'],
+        Page_Results: ['BreakVariables'],
+
+    }
+
+}
