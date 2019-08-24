0e242d510cebfa69a69946a2143872b211732b98
EkaterinaT-CIT <ekaterina.tarakanova@confirmit.com>
2019-08-24 20:11:46 +0300

    update pulse content survey info

--- __PulseSurveysInfoFabric.js
+++ __PulseSurveysInfoFabric.js
@@ -1,6 +1,8 @@
 class PulseSurveysInfoFabric {
 
     static function getPulseSurveysInfo(context, storageInfo) {
-        return PulseSurveys_ReportalTable.getInstance(context, storageInfo);
+        // for now the table is built manually, but when custom table synch is fixed, data will be taken from DB table
+        // in addition to this method
+        return PulseSurveys_ReportalTable.getInstance(context, storageInfo); 
     }
 }
