2cbe9bdf8e8de6245a6462f12e8d0dc3914f64b2
EkaterinaT-CIT <ekaterina.tarakanova@confirmit.com>
2019-08-24 17:56:49 +0300

    oulse data

--- __SpecificPulseSuveyData.js
+++ __SpecificPulseSuveyData.js
@@ -1,7 +1,5 @@
 class SpecificPulseSurveyData {
 
-    static public var resourcesInfo = {};
-
     static private var resourcesDependentOnSpecificSurvey = {
 
         Survey: ['FiltersFromSurveyData'],
@@ -17,41 +15,50 @@ class SpecificPulseSurveyData {
     /**
      * @param {Object} context
      * @param {string} pageId - not mandatory
-     * @returns {Object} object where property is resourceId (question or dimension) and value is its type
+     * @returns {Array} object where property is resourceId (question or dimension) and value is its type
      */
-    static private function setResourcesInfoForCurrentPage (context, pageId) {
+    static private function getResourcesList (context, pageId) {
 
-        var currentPage = pageId ? 'Page_'+pageId : 'Page_'+context.pageContext.Items['CurrentPageId'];
         var listOfResources = [];
-        var resources = {};
-
-        //var take survey level resources
-        var surveyProperties = resourcesDependentOnSpecificSurvey['Survey'];
-        var pageProperties = resourcesDependentOnSpecificSurvey[currentPage];
+        var resources = [];
+        var resourcesLog = {};
+        
+        pageId = (pageId) ? 'Page_'+pageId : 'Page_'+context.pageContext.Items['CurrentPageId'];
+        
         var i;
+        var surveyProperties = resourcesDependentOnSpecificSurvey['Survey'];
+        var pageProperties = resourcesDependentOnSpecificSurvey[pageId];
 
-        // push survey level variables
-        for(i=0; i< surveyProperties.length; i++) {
+        // keep property values in array
+        for(i=0; i<surveyProperties.length; i++) {
             listOfResources=listOfResources.concat(DataSourceUtil.getSurveyPropertyValueFromConfig (context, surveyProperties[i]));
         }
 
-        //push page level variables
-        for(i=0; i< pageProperties.length; i++) {
-            listOfResources=listOfResources.concat(DataSourceUtil.getPagePropertyValueFromConfig (context, currentPage, pageProperties[i]));
+        for(i=0; i<pageProperties.length; i++) {
+            listOfResources=listOfResources.concat(DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, pageProperties[i]));
         }
 
         //remove duplicates and format
         for(i=0; i<listOfResources.length; i++) {
             var item = listOfResources[i];
+            var code;
+            var type;
 
             if(typeof item === 'string') {
-                resources[item] = 'QuestionId';
-            } else {
-                resources[item.Code] = item.Type; //dimension
+                code = item;
+                type = 'QuestionId';
+            } else { //dimension
+                //code = item.Code;
+                //type = item.Type; 
+            }
+
+            if(code && !resourcesLog.hasOwnProperty(code)) {
+                resources.push({Code: code, Type: type});
+                resourcesLog[code] = true;
             }
         }
 
-        resourcesInfo = resources;
+        return resources;        
     }
 
     /**
@@ -63,29 +70,28 @@ class SpecificPulseSurveyData {
         var log = context.log;
         var report = context.report;
         var table = context.table;
-        
-        setResourcesInfoForCurrentPage(context, pageId);
+        var resources = getResourcesList(context);
 
-        var resources = resourcesInfo;
+        for(var i=0; i< resources.length; i++) {
 
-        for(var resourceID in resources) {
+            var resource = resources[i];
 
             var base: HeaderBase = new HeaderBase();
             var header;
 
-            if(resources[resourceID] === 'Dimension') { //category;            
+            if(resource.Type === 'Dimension') { //category;            
               
                 header = new HeaderCategorization();
-                header.CategorizationId = resourceID;
+                header.CategorizationId = resource.Code;
                 header.DataSourceNodeId = DataSourceUtil.getDsId(context);
                 header.Collapsed = true;
                 header.Totals = true;
                 table.RowHeaders.Add(header); // to avoid case when previous header is added if troubles 
 
-            } else if (resources[resourceID] === 'QuestionId') {  // question id
+            } else if (resource.Type === 'QuestionId') {  // question id
 
-                var questionInfo = QuestionUtil.getQuestionInfo(context, resourceID);
-                var qe: QuestionnaireElement =  QuestionUtil.getQuestionnaireElement(context, resourceID);
+                var questionInfo = QuestionUtil.getQuestionInfo(context, resource.Code);
+                var qe: QuestionnaireElement =  QuestionUtil.getQuestionnaireElement(context, resource.Code);
                 var questionType;
 
                 //define question type to apply correct header properties later
