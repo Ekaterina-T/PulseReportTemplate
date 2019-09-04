e7e71ca109ae3881d1292d839e9fbd8614f414ac
EkaterinaT <Murzik17>
2019-09-04 11:04:04 +0300

    debug user

--- __User.js
+++ __User.js
@@ -10,22 +10,26 @@ class User {
         var log = context.log;
         var user = context.user;
 
+        log.LogDebug('1')
         //confirmit user (has access to all surveys for QA) or survey is available for all end users (validRoles is null)
         if(user.UserType === ReportUserType.Confirmit || !validRoles) {
             return true;
         }
 
+        log.LogDebug('2')
         // survey is not available for any role
         if(validRoles.length === 0) {
             return false;
         }
 
+        log.LogDebug('3')
         for(var i=0; i<validRoles.length; i++) {
             if(user.HasRole(validRoles[i])) {
                 return true;
             }
         }
 
+        log.LogDebug('4')
         return false;
 
     }
