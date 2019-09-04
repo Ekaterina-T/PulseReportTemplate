c27060f784b2be249fa83c380cb65a4277b46961
EkaterinaT <Murzik17>
2019-09-04 13:30:01 +0300

    debug user

--- __PageUtil.js
+++ __PageUtil.js
@@ -16,13 +16,17 @@ class PageUtil {
         pageContext.Items.Add('userEmail', context.user.Email);
         pageContext.Items.Add('CurrentPageId', page.CurrentPageId);
 
+        log.LogDebug(page.CurrentPageId+': 1')
+
         try {
             var add_in_source = DataSourceUtil.getPagePropertyValueFromConfig(context, page.CurrentPageId, 'Source');
             pageContext.Items.Add('Source', add_in_source);
         }
         catch (e) { /* 'Source' is optional page property which allows to use different sources for specific pages. So no need for throwing errors  ' */}
+        log.LogDebug(page.CurrentPageId+': 2')
 
         ParamUtil.Initialise(context); // initialise parameters
+        log.LogDebug(page.CurrentPageId+': 3')
 
         // if in current DS a page shouldn't be visible, than redirect to default page
         // very actual when 1st report page should not be visible
@@ -30,6 +34,7 @@ class PageUtil {
             page.NextPageId = DataSourceUtil.getSurveyPropertyValueFromConfig (context, 'DefaultPage');
             return;
         }
+        log.LogDebug(page.CurrentPageId+': 4')
 
         // reset not bg var based filters on response rate page
         if(pageContext.Items['CurrentPageId'] === 'Response_Rate') {
@@ -41,12 +46,14 @@ class PageUtil {
                 state.Parameters['p_ScriptedFilterPanelParameter'+(filterFromRespondentData.length+i+1)] = null;
             }
         }
+        log.LogDebug(page.CurrentPageId+': 5')
 
         if(!HierarchyUtil.Hide(context) && HierarchyUtil.isDataTableEmpty(context)) { // hierarchy needed and not cached yet
             // populate cached hierarchy if needed
             // for now it's only needed for results page hierarchy benchamrks
             HierarchyUtil.setDataTable(context);
         }
+        log.LogDebug(page.CurrentPageId+': 6')
 
         //for tests
         PulseProgramUtil.printPulseSurveyContentInfoTable(context);
