767e8697ce447ee43e02dfe7aa0b626fe0f7c166
EkaterinaT <Murzik17>
2019-09-04 11:55:24 +0300

    debug user

--- __User.js
+++ __User.js
@@ -10,10 +10,6 @@ class User {
         var log = context.log;
         var user = context.user;
 
-        log.LogDebug('1 '+from)
-        log.LogDebug('validRoles'+JSON.stringify(validRoles))
-        log.LogDebug('11')
-        log.LogDebug('user'+context.hasOwnProperty('user'))
         //confirmit user (has access to all surveys for QA) or survey is available for all end users (validRoles is null)
         if(user.UserType === ReportUserType.Confirmit || !validRoles) {
             return true;
