745d0a64fca7cc84a200e8b1786c48e8be539217
EkaterinaT-CIT <ekaterina.tarakanova@confirmit.com>
2019-12-08 18:15:17 +0300

    add logging in PulseSurveys_Table

--- __PulseSurveys_ReportalTable.js
+++ __PulseSurveys_ReportalTable.js
@@ -31,17 +31,22 @@ public class PulseSurveys_ReportalTable implements IPulseSurveysInfo {
 
         var report = context.report;
         var log = context.log;
-        var rawInfo = report.TableUtils.GetRowHeaderCategoryTitles(_pulseSurveysTablePath);
+        var rowInfo = report.TableUtils.GetRowHeaderCategoryTitles(_pulseSurveysTablePath);
         var surveyList = [];
 
-        if(_isEmptyOptionNeeded) {
+        if(_isEmptyOptionNeeded) { // to do: move to system config for consistency?
             var emptyOption = {};
             emptyOption.Label = TextAndParameterUtil.getTextTranslationByKey(context, 'SelectSurveyEmptyOption');
             emptyOption.Code = 'none';
             surveyList[0] = emptyOption;
         }
 
-        surveyList = surveyList.concat(transformTableHeaderTitlesIntoObj(context, rawInfo));
+        var rowInfo2 = report.TableUtils.GetColumnHeaderCategoryIds(_pulseSurveysTablePath);
+        log.LogDebug('category titles: '+JSON.stringify(rowInfo));
+        log.LogDebug('category titles: '+JSON.stringify(rowInfo2));
+        
+
+        surveyList = surveyList.concat(transformTableHeaderTitlesIntoObj(context, rowInfo));
 
         return surveyList;
     }
@@ -60,8 +65,8 @@ public class PulseSurveys_ReportalTable implements IPulseSurveysInfo {
         for(var i=HeaderCategoryTitles.length-1; i>=0; i--) { // reverse order
             var surveyInfo = {};
 
-            var headerRow = HeaderCategoryTitles[i];
-            var colNum = headerRow.length;
+            var headerRow = HeaderCategoryTitles[i]; // all headers in the row
+            var colNum = headerRow.length; // number of columns
 
             var sureveyId = headerRow[colNum-1];
             var surveyName = headerRow[colNum-2];
