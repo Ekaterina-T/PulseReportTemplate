a87d4278c2c837a04b3e78024faa78a023bf8ac8
EkaterinaT <Murzik17>
2020-01-14 17:00:02 +0300

    discard tracker changes

--- __SurveyTracker.js
+++ __SurveyTracker.js
@@ -1,72 +1,25 @@
 class SurveyTracker {
 
     /**
-    * Gets TrackerString property from Config
-    * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
-    * @return {Array} - tracker surveys related to the selected pulse survey
-    */
-    static function getTrackerStringFromConfig(context, trackerStringId) {
+     * Gets TrackerString property from Config
+     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
+     */
+    static function getTrackerStringFromConfig(context) {
         var log = context.log;
-
-        //only relevant for pulse programs
-        if(DataSourceUtil.isProjectSelectorNotNeeded(context)) {
-            return [];
+        var pageContext = context.PageContext;
+        if (DataSourceUtil.getSurveyPropertyValueFromConfig (context, 'TrackerString')) {
+            return DataSourceUtil.getSurveyPropertyValueFromConfig (context, 'TrackerString');
         }
-
-        //search for trackerStringId - pid, if nothing else specified explicitly
-        if(!trackerStringId) {
-            trackerStringId = ParamUtil.GetSelectedCodes(context, 'p_projectSelector');
+        else {
+            throw new Error('TrackerString was not defined in Config!');
         }
-
-        var trackerStrings = DataSourceUtil.getSurveyPropertyValueFromConfig (context, 'TrackerStrings');
-
-        if(trackerStrings.hasOwnProperty(trackerStringId)) {
-            return trackerStrings[trackerStringId];
-        } else {
-            return [];
-        }
-    }
-
-    /**
-     * Gets list of all pulse surveys from the allSurveys table in SystemPages -> PulseSurveyData -> AllSurveys table
-     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
-     * @returns {Array} array of objects like {Code: pid, Label: pname}
-     */
-    static function getAllSurveyDescriptors(context) {
-        return PulseSurveysInfoFabric.getPulseSurveysInfo(context,  {
-            type: 'ReportTable',
-            tableName:'PulseSurveyData:AllSurveys_PidPname',
-            isEmptyOptionNeeded: false,
-            additionalInfo: {'CreatedByEndUserName': false, 'Status': false}// order is important, do not delete anything
-        }).getPulseSurveys(context);
-    }
-
-    /**
-     * Gets Code and Label from list of all surveys - in case we change object which getAllSurveyNames function returns
-     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
-     * @param {string} pid - contains id of the survey for which we get its name
-     * @returns {object} {Code: pid, Label: pname}
-     */
-    static function getSurveyDescriptorByPid(context, pid) {
-        var allDescriptors = getAllSurveyDescriptors(context);
-        var surveyDescriptor = {};
-
-        for (var i = 0; i < allDescriptors.length; i++) {
-            if (allNames[i].Code == pid) {
-                surveyDescriptor.Label = allDescriptors[i].Label;
-                surveyDescriptor.Code = allDescriptors[i].Code;
-                break;
-            }
-        }
-        return surveyDescriptor;
     }
 
     /**
      * Gets one previous survey for survey with id = trackerId from TrackerString property in Config.
      * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
      * @param {string} trackerId - contains id of the survey for which we look for previous surveys
-     * @returns {object} survey descriptor {Code pid, Label: pname}
-    */
+     */
     static function getOnePreviousSurvey(context, trackerId) {
         var allSurveys = getTrackerStringFromConfig(context);
         var currentTrackerIndex = -1;
@@ -79,68 +32,95 @@ class SurveyTracker {
             }
         }
 
-        if(currentTrackerIndex != -1 && (currentTrackerIndex + 1) < allSurveys.length) {
-            previousSurveyId = allSurveys[currentTrackerIndex+1];
-        } else {
-            return null; //previous survey is undefined
+        if (currentTrackerIndex != -1) {
+            for (var j = currentTrackerIndex + 1; j < allSurveys.length; j++) {
+                if (allSurveys[j] != '') {
+                    previousSurveyId = allSurveys[j];
+                    break;
+                }
+            }
         }
-
-        return getSurveyDescriptorByPid(context, previousSurveyId);
+        return getSurveyName(context, previousSurveyId);
     }
 
     /**
-    * Gets one previous survey to survey selected in report dropdown.
-    * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
-    */
+     * Gets all previous surveys for survey with id = trackerId from TrackerString property in Config.
+     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
+     * @param {string} trackerId - contains id of the survey for which we look for previous surveys
+     */
+    static function getAllPreviousSurveys(context, trackerId) {
+        var allSurveys = getTrackerStringFromConfig(context);
+        var currentTrackerIndex = -1;
+        var previousSurveyIds = [];
+
+        for (var i = 0; i < allSurveys.length; i++) {
+            if (allSurveys[i] == trackerId) {
+                currentTrackerIndex = i;
+                break;
+            }
+        }
+
+        var previousSurveys = [];
+        if (currentTrackerIndex != -1) {
+            for (var j = currentTrackerIndex + 1; j < allSurveys.length; j++) {
+                if (allSurveys[j] != '') {
+                    previousSurveyIds.push(allSurveys[j]);
+                }
+            }
+            for (var i = 0; i < previousSurveyIds.length; i++) {
+                previousSurveys.push(getSurveyName(previousSurveyIds[i]));
+            }
+        }
+        return previousSurveys;
+    }
+
+    /**
+     * Gets one previous survey to survey selected in report dropdown.
+     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
+     */
     static function getPreviousSurveyToSelected(context) {
         var projectSelected = ParamUtil.GetSelectedCodes(context, 'p_projectSelector');
         return getOnePreviousSurvey(context, projectSelected[0]);
     }
 
-
     /**
-    * Gets all previous surveys for survey with id = trackerId from TrackerString property in Config.
-    * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
-    * @param {string} trackerId - contains id of the survey for which we look for previous surveys
-    */
-    /*
-    static function getAllPreviousSurveys(context, trackerId) {
-    var allSurveys = getTrackerStringFromConfig(context);
-    var currentTrackerIndex = -1;
-    var previousSurveyIds = [];
-
-    for (var i = 0; i < allSurveys.length; i++) {
-    if (allSurveys[i] == trackerId) {
-      currentTrackerIndex = i;
-      break;
-    }
-    }
-
-    var previousSurveys = [];
-    if (currentTrackerIndex != -1) {
-    for (var j = currentTrackerIndex + 1; j < allSurveys.length; j++) {
-    if (allSurveys[j] != '') {
-        previousSurveyIds.push(allSurveys[j]);
-    }
-    }
-    for (var i = 0; i < previousSurveyIds.length; i++) {
-    previousSurveys.push(getSurveyName(previousSurveyIds[i]));
-    }
-    }
-    return previousSurveys;
-    }
-    */
-
-    /**
-    *  Gets all previous surveys to survey selected in report dropdown.
-    * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
-    */
-    /*
+     *  Gets all previous surveys to survey selected in report dropdown.
+     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
+     */
     static function getAllPreviousSurveysToSelected(context) {
-    var projectSelected = ParamUtil.GetSelectedCodes(context, 'p_projectSelector');
-    return getAllPreviousSurveys(context, projectSelected[0]);
+        var projectSelected = ParamUtil.GetSelectedCodes(context, 'p_projectSelector');
+        return getAllPreviousSurveys(context, projectSelected[0]);
     }
 
-    */
+    /**
+     * Gets Code and Label from list of all surveys - in case we change object which getAllSurveyNames function returns
+     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
+     * @param {string} pid - contains id of the survey for which we get its name
+     */
+    static function getSurveyName(context, pid) {
+        var allNames = getAllSurveyNames(context);
+        var filteredOption = {};
 
+        for (var i = 0; i < allNames.length; i++) {
+            if (allNames[i].Code == pid) {
+                filteredOption.Label = allNames[i].Label;
+                filteredOption.Code = allNames[i].Code;
+                break;
+            }
+        }
+        return filteredOption;
+    }
+
+    /**
+     * Gets list of all pulse surveys from the allSurveys table in SystemPages -> PulseSurveyData -> AllSurveys table
+     * @param {object} context - contains Reportal scripting state, log, report, user, parameter objects
+     */
+    static function getAllSurveyNames(context) {
+        return PulseSurveysInfoFabric.getPulseSurveysInfo(context,  {
+            type: 'ReportTable',
+            tableName:'PulseSurveyData:AllSurveys_PidPname',
+            isEmptyOptionNeeded: false,
+            additionalInfo: {'CreatedByEndUserName': false, 'Status': false}// order is important, do not delete anything
+        }).getPulseSurveys(context);
+    }
 }
