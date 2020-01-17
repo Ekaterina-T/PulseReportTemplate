6414be9aa4f8b77bc3917e73ccd86311882da060
EkaterinaT <Murzik17>
2020-01-17 19:46:11 +0300

    KN-429: hide custom tab when it's not needed

--- __ParamUtil.js
+++ __ParamUtil.js
@@ -368,6 +368,7 @@ class ParamUtil {
 
             //hide custom tab when it's not needed
             var isPulseProgram = !DataSourceUtil.isProjectSelectorNotNeeded(context);
+            var pageId = PageUtil.getCurrentPageIdInConfig(context);
             var custom_category = DataSourceUtil.getPagePropertyValueFromConfig(context, pageId, 'CustomStatementCategory');
 
             if(!isPulseProgram || !custom_category) {
