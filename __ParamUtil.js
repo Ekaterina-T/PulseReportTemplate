75e5b7f6f9b4d3756908a0ae079d4873754937cb
EkaterinaT <Murzik17>
2020-01-29 19:18:28 +0300

    mass export by pulse project

--- __ParamUtil.js
+++ __ParamUtil.js
@@ -29,15 +29,12 @@ class ParamUtil {
 
         var parameter = context.parameter;
         var log = context.log;
-        log.LogDebug('LoadParameter 1');
 
         if (!isParameterToBeLoaded(context)) { // no need to load parameter
             return;
         }
-        log.LogDebug('LoadParameter 2');
 
         var parameterOptions = ParameterOptions.GetOptions(context, null, 'load'); // get options
-        log.LogDebug('LoadParameter 3');
 
         for (var i = 0; i < parameterOptions.length; i++) { // populate parameter
             var val = new ParameterValueResponse();
@@ -45,7 +42,6 @@ class ParamUtil {
             val.StringValue = parameterOptions[i].Label;
             parameter.Items.Add(val);
         }
-        log.LogDebug('LoadParameter 4');
 
         return;
     }
@@ -360,14 +356,12 @@ class ParamUtil {
         var log = context.log;
 
         var isPulseProgram = !DataSourceUtil.isProjectSelectorNotNeeded(context);
-        log.LogDebug('isParameterToBeLoaded 1: '+isPulseProgram);
 
         if (parameterName === 'p_projectSelector') {
             return isPulseProgram;
         }
 
         var pageId = PageUtil.getCurrentPageIdInConfig(context);
-        log.LogDebug('isParameterToBeLoaded 2: '+pageId);
 
         if (parameterName === 'p_Results_CountsPercents') {
             var user = context.user;
@@ -435,8 +429,6 @@ class ParamUtil {
         if (parameterName === 'p_BenchmarkSet') {
             return DataSourceUtil.getPagePropertyValueFromConfig(context, 'Page_Results', 'BenchmarkSet') ? true : false;
         }
-
-        log.LogDebug('isParameterToBeLoaded 3: '+pageId);
 
         if(pageId === 'Page_Trends' && isPulseProgram && parameterName === 'p_DisplayMode') {
             return false;
