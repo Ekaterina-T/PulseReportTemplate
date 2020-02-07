01121a10358303b3f306453f0117e5e5dd238644
EkaterinaT <Murzik17>
2020-02-07 15:33:32 +0300

    mass export by particular pid

--- __Export.js
+++ __Export.js
@@ -7,7 +7,7 @@ class Export {
 
     static function isExcelExportMode (context) {
         var state = context.state;
-        return state.ReportExecutionMode === ReportExecutionMode.ExcelExport;
+        return displayDataSourceInfo;
     }
 
     static function isPdfExportMode (context) {
@@ -26,14 +26,19 @@ class Export {
         var log = context.log;
         var str = '';
 
+        log.LogDebug('displayDataSourceInfo 1');
+
         if(Config.Surveys.length>1) {
             var selectedProject: Project = DataSourceUtil.getProject(context);
             str+='Program Name: '+selectedProject.ProjectName+' ';
             str += System.Environment.NewLine; // for Excel export
         }
+        log.LogDebug('displayDataSourceInfo 2');
 
         if(!state.Parameters.IsNull('p_projectSelector')) {
+            log.LogDebug('displayDataSourceInfo 3 '+JSON.stringify(ParamUtil.GetSelectedOptions (context, 'p_projectSelector')));
             var selectedSurvey = ParamUtil.GetSelectedOptions (context, 'p_projectSelector')[0];
+            log.LogDebug('displayDataSourceInfo 4');
             if(selectedSurvey.Code!=='none') {
                 str+= 'Survey Name: '+selectedSurvey.Label+' ';
                 str = '<div class="data-source-info">'+str+'</div>';
