5b8c0dcd377eeb2e15f6f1e76d5ac1b92ede1eba
Namioko <galaida_anna@inbox.ru>
2020-03-17 17:06:59 +0300

    SystemConfig - add information for p_Hierarchy

--- ___SystemConfig.js
+++ ___SystemConfig.js
@@ -44,7 +44,8 @@ public class SystemConfig {
         'p_OnlyOwnActions':     { type: 'StaticArrayofObjects', locationType: 'TextAndParameterLibrary',    propertyName: 'ShowOnlyOwnActions'},
 
         'p_TrendQs': { type: 'QuestionAndCategoriesList', locationType: 'Page', page: 'Page_Trends', propertyName: 'TrendQuestions', isQuestionBased: true },
-        'p_Wave': { type: 'QuestionId', locationType: 'Survey', propertyName: 'WaveQuestion', isInReverseOrder: true}
+        'p_Wave': { type: 'QuestionId', locationType: 'Survey', propertyName: 'WaveQuestion', isInReverseOrder: true},
+        'p_Hierarchy': { type: 'QuestionId', locationType: 'Survey', propertyName: 'HierarchyTableQuestion', isInReverseOrder: false}
 
     };
 
