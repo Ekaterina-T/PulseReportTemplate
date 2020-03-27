acfeebdc07c2dcb553d24ee737fbd082e7b0c09a
victoria.karpova@confirmit.com <victoria.karpova@confirmit.com>
2020-03-27 21:23:25 +0300

    UserIds changes

--- __PageActions.js
+++ __PageActions.js
@@ -42,8 +42,8 @@ class PageActions {
            throw new Error('PageActions.ActionBtn_Render: User is not in the table ' + tableName +' in schema ' + schemaId);
         }
 
-        linkParameters.push('B='+  BranchSpecifics.getSelectedBranchId(context));
-        linkParameters.push('Id=' + endUserIds[0]);
+        //linkParameters.push('B='+  BranchSpecifics.getSelectedBranchId(context));
+        //linkParameters.push('Id=' + endUserIds[0]);
         linkParameters.push('U=' +  user.UserId);
 
         if(!HierarchyUtil.Hide(context)) linkParameters.push('hier='+ user.PersonalizedReportBase);
