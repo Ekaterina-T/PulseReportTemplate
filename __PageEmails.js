31edf376be24b01d5842f92974c1e6191487bae7
thequeen14 <flamina96@yandex.ru>
2020-04-22 09:40:29 +0300

    IF-66: Add Emails page

---
+++ __PageEmails.js
@@ -0,0 +1,100 @@
+class PageEmails {
+    
+        /**
+         * @memberof PageEmails
+         * @function Hide
+         * @description function to hide the page
+         * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+         * @returns {Boolean}
+         */
+        static function Hide(context) {
+            return false;
+        }
+    
+        /**
+         * @memberof PageEmails
+         * @function Render
+         * @description function to render the page
+         * @param {Object} context - {component: page, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+         */
+        static function Render(context){
+    
+        }
+    
+        /**
+         * @memberof PageEmails
+         * @function hitlistComments_Hide
+         * @description function to hide the hitlist
+         * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+         * @returns {Boolean}
+         */
+        static function hitlistComments_Hide(context){
+           var log = context.log;
+          return Hitlist.hitlistComments_Hide(context, "Base");
+        }
+    
+        /**
+         * @memberof PageEmails
+         * @function hitlistComments_Render
+         * @description function to render the hitlist
+         * @param {Object} context - {component: hitlist, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+         */
+        static function hitlistComments_Render(context){
+    
+            var log = context.log;
+            var pageId = PageUtil.getCurrentPageIdInConfig(context);
+    
+            var staticCols = DataSourceUtil.getPagePropertyValueFromConfig (context, pageId, 'staticColumns');
+    
+            for (var i=0; i<staticCols.length; i++) {
+                Hitlist.AddColumn(context, staticCols[i], {sortable: true, searchable: true});
+            }
+        }
+    
+         /**
+         * @memberof PageEmails
+         * @function tableBase_Render
+         * @description function to render the Base table used to check required base for Hitlist
+         * @param {Object} context - {component: hitlist, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}     
+         */
+          static function tableBase_Render (context) {
+              var log = context.log;
+              var open_Ids = ParamUtil.GetSelectedCodes (context, 'p_AllOpenTextQs');
+              var tag_Ids = ParamUtil.GetSelectedCodes (context, 'p_TagQs');
+              SuppressUtil.buildReportBaseTableForHitlist(context, open_Ids, tag_Ids);
+          }
+  
+  
+         /**
+         * @memberof PageEmails
+         * @function tableEmailStatusSummary_Render
+         * @description function to render the Email Status table used to show short summary of email statuses
+         * @param {Object} context - {component: hitlist, pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}     
+         */
+          static function tableEmailStatusSummary_Render (context) {
+            var log = context.log;
+            var table = context.table;
+            var suppressSettings = context.suppressSettings;
+               
+            var questionnaireElement: QuestionnaireElement = QuestionUtil.getQuestionnaireElement(context, 'smtpstatus');
+            var headerQuestion: HeaderQuestion = new HeaderQuestion(questionnaireElement);
+            headerQuestion.IsCollapsed = false;
+            TableUtil.maskOutNA(context, headerQuestion);
+            table.Decimals = 0;
+            table.RowHeaders.Add(headerQuestion);
+            SuppressUtil.setTableSuppress(table, suppressSettings);
+          }
+  
+    /**
+    * @memberof PageEmails
+    * @function tableEmailStatusSummary_Hide
+    * @description function to hide the EmailStatusSummary table
+    * @param {Object} context - {pageContext: this.pageContext, report: report, user: user, state: state, confirmit: confirmit, log: log}
+    * @returns {Boolean}
+    */
+    static function tableEmailStatusSummary_Hide(context){
+
+     return SuppressUtil.isGloballyHidden(context);
+
+   }
+}
