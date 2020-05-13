3b6ee6b503a9f11dfb8498ab506ab10dbc60a258
EkaterinaT <Murzik17>
2020-05-13 17:25:41 +0300

    IF-129: selectable rows in All Results table

--- ___SystemConfig.js
+++ ___SystemConfig.js
@@ -51,7 +51,7 @@ public class SystemConfig {
     static var mandatoryPageParameters = ['p_projectSelector', 'p_TimeUnitWithDefault', 'p_TimePeriod', 'p_BenchmarkSet',
                                           'p_Wave', 'p_OpenTextQs', 'p_CustomOpenTextQs', 'p_AllOpenTextQs', 'p_TrendQs',
                                           'p_Demographics', 'p_QsToFilterBy', 'p_Dimensions', 'p_Results_TableTabSwitcher',
-                                            'p_OrgOverviewBreakBy'];
+                                            'p_OrgOverviewBreakBy', 'p_AllResults_BreakBy'];
 
     // optional parameters are usually multiple. Can be empty by default
     static var optionalPageParameters = ['p_ScoreQs', 'p_TagQs', 'p_TimeUnitNoDefault', 'p_CatDD_TimeUnitNoDefault']; // we must add them empty option as 1st value instead
