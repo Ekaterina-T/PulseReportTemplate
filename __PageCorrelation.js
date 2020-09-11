ca76c5f22cede1a7ea5bbbd955d040acd9fa0e79
ElenaV <helenavlasova39@gmail.com>
2020-09-11 15:49:37 +0300

    tableCorrelation_Render + additional functions for it in TableUtil (conditional formatting, row sorting)

--- __PageCorrelation.js
+++ __PageCorrelation.js
@@ -13,12 +13,6 @@ class PageCorrelation {
 
         //SuppressUtil.setTableSuppress(table, suppressSettings);
 
-        var selectedCorrelationVariable = ParamUtil.GetSelectedOptions(context, 'p_CorrelationQuestion')[0];
-
-        if(!!selectedCorrelationVariable) {
-            var selectedQuestion = selectedCorrelationVariable.Code;
-        }
-
         tableCorrelation_AddRows(context);
         tableCorrelation_AddColumns(context);
         tableCorrelation_AddConditionalFormatting(context);
@@ -82,15 +76,115 @@ class PageCorrelation {
      * @param {object} context: {state: state, report: report, log: log, table: table, pageContext: pageContext, suppressSettings: suppressSettings}
      */
     static function tableCorrelation_AddColumns(context) {
+        var log = context.log;
+        var table = context.table;
 
+        var columnsCollection: HeaderCollection = new HeaderCollection();
+        columnsCollection.Add(getStatisticsColumn());
+        columnsCollection.Add(getCorrelation());
+        columnsCollection.Add(getBaseColumn());
+        columnsCollection.Add(getFormulaColumn());
+
+        table.ColumnHeaders.AddRange(columnsCollection);
+    }
+
+    /*
+     * Create HeaderStatistics column
+     * @return {HeaderStatistics} created column
+     */
+    static function getStatisticsColumn() {
+        var headerStatistics = new HeaderStatistics();
+        headerStatistics.Statistics.Avg = true;
+
+        return headerStatistics
+    }
+
+    /*
+     * Create HeaderCorrelation column based on the selected correlation question
+     * @return {HeaderCorrelation} created column
+     */
+    static function getCorrelation() {
+        var selectedCorrelationVariable = ParamUtil.GetSelectedOptions(context, 'p_CorrelationQuestion')[0];
+
+        if(!!selectedCorrelationVariable) {
+            var selectedQuestion = selectedCorrelationVariable.Code;
+            var questionnaireElement: QuestionnaireElement = QuestionUtil.getQuestionnaireElement(context, selectedQuestion);
+
+            var headerCorrelation : HeaderCorrelation = new HeaderCorrelation(CorrelationType.Correlation, questionnaireElement);
+            headerCorrelation.Decimals = 3;
+
+            return headerCorrelation;
+        } else {
+            throw new Error("PageCorrelation.tableCorrelation_AddColumns.getCorrelation: no correlation question specified in the Config");
+        }
+    }
+
+    /*
+     * Create HeaderBase column
+     * @return {HeaderBase} created column
+     */
+    static function getBaseColumn() {
+        var headerBase: HeaderBase = new HeaderBase();
+
+        return headerBase;
+    }
+
+    /*
+     * Create HeaderFormula column which is calculated based on AdjustedSentiment
+     * if categorySentiment < overallSentiment then Adjusted sentiment = (10-categorySentiment+5), else Adjusted sentiment = (categorySentiment+5)
+     * final value is AdjustedSentiment * Counts * Correlation
+     * @return {HeaderFormula} created column
+     */
+    static function getFormulaColumn() {
+        var headerFormula: HeaderFormula = new HeaderFormula();
+        headerFormula.Type = FormulaType.Expression;
+        headerFormula.Expression = "(IF(cellv(col-3,row)<cellv(col-3,1), (10 - (cellv(col-3,row) + 5)), (cellv(col-3,row) + 5))) * cellv(col-2,row) * cellv(col-1, row)";
+        headerFormula.HideData = false;
+
+        return headerFormula;
     }
 
     /*
      * Apply conditional formatting to the Correlation table to section the table into areas
+     * Conditional formatting is used to define key area for each category
+     *      1. issues: categorySentiment <= overallSentiment && correlation > 0
+     *      2. monitor: categorySentiment < overallSentiment && correlation <= 0
+     *      3. strength: categorySentiment > overallSentiment && correlation >0
+     *      4. maintain: ctegorySentiment > overallSentiment && correlation <=0
+     * conditional formatting applied to the firs column and then used by js fuction to create chart and 4 different tables
      * @param {object} context: {state: state, report: report, log: log, table: table, pageContext: pageContext, suppressSettings: suppressSettings}
      */
     static function tableCorrelation_AddConditionalFormatting(context) {
+        var log = context.log;
+        var table = context.table;
 
+        var conditions = [
+            {
+                expression: 'cellv(col + 1, row)>0 AND cellv(col,row) <= cellv(col, 1) ',
+                style: 'issues'
+            },
+
+            {
+                expression: '(cellv(col + 1, row) = EMPTYV() OR cellv(col + 1, row)<=0) AND cellv(col,row) <= cellv(col, 1) ',
+                style: 'monitor'
+            },
+            {
+                expression: 'cellv(col + 1, row)>0 AND cellv(col,row) > cellv(col, 1) ',
+                style: 'strength'
+            },
+            {
+                expression: '(cellv(col + 1, row) = EMPTYV() OR cellv(col + 1, row)<=0) AND cellv(col,row) > cellv(col, 1) ',
+                style: 'maintain'
+            }
+        ];
+        var name = "KeyAreas";
+        var applyTo = {
+            axis: Area.Columns,
+            direction: Area.Left,
+            indexes: "1"
+        };
+
+        TableUtil.setupConditionalFormatting(context, conditions, name, applyTo);
     }
 
     /*
@@ -98,6 +192,6 @@ class PageCorrelation {
      * @param {object} context: {state: state, report: report, log: log, table: table, pageContext: pageContext, suppressSettings: suppressSettings}
      */
     static function tableCorrelation_AddSorting(context) {
-
+        TableUtil.setupRowsTableSorting(context, false, 1, 0, true, 1);
     }
 }
