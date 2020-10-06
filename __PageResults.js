fb2b6d6fa7bbe3c40e17dd0daf6d94cb2a5c6f24
ElenaV <helenavlasova39@gmail.com>
2020-10-06 13:47:08 +0300

    do not copy company-total comparison if top node for hierarchy

--- __PageResults.js
+++ __PageResults.js
@@ -871,12 +871,9 @@ class PageResults {
 
                 var toCopy = true;
 
-                log.LogDebug(HierarchyUtil.getTopNode(context));
-                log.LogDebug(reportBases);
-
                 if(hierCompCols[i] === 'company-total') {
                     var companyTotals = HierarchyUtil.getReferencedNodeValuesForCurrentReportBase(context, Config.companyTotalField);
-                    if(!(companyTotals && companyTotals.length === 1)) {
+                    if(!(companyTotals && companyTotals.length === 1) || reportBases[0] === HierarchyUtil.getTopNode(context)) {
                         toCopy = false;
                     }
                 }
