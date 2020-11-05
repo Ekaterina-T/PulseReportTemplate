0d9d6e9fb6b98a591a1e2d2a0027ef5b40b29da6
ElenaM717 <elena_asz@mail.ru>
2020-11-05 12:29:19 +0300

    IF-202: PageKPI - add waves to OrgOverview table

--- ___SystemConfig.js
+++ ___SystemConfig.js
@@ -63,9 +63,9 @@ public class SystemConfig {
     static var mandatoryPageParameters = ['p_projectSelector', 'p_TimeUnitWithDefault', 'p_TimePeriod', 'p_BenchmarkSet',
                                           'p_Wave', 'p_OpenTextQs', 'p_CustomOpenTextQs', 'p_AllOpenTextQs', 'p_TrendQs',
                                           'p_Demographics', 'p_QsToFilterBy', 'p_Dimensions', 'p_Results_TableTabSwitcher',
-                                          'p_OrgOverviewBreakBy', 'p_AllResults_BreakBy',
-                                          'p_ImpactAnalysisDimension', 'p_CorrelationQuestion', 'p_WaveSelector', 'p_LeadersExport_BreakBy',
-                                          'p_LeadersExport_Dimension'];
+                                          'p_OrgOverviewBreakBy', 'p_AllResults_BreakBy', 'p_WaveOrgOverview', 
+                                          'p_ImpactAnalysisDimension', 'p_CorrelationQuestion', 'p_WaveSelector', 
+                                          'p_LeadersExport_BreakBy', 'p_LeadersExport_Dimension'];
 
     // optional parameters are usually multiple. Can be empty by default
     static var optionalPageParameters = ['p_ScoreQs', 'p_TagQs', 'p_TimeUnitNoDefault', 'p_CatDD_TimeUnitNoDefault', 'p_AllResults_GapSettings']; // we must add them empty option as 1st value instead
