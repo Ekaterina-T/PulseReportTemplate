346106bed2158c1a5cf555945c2f66f5779d84d7
ElenaV <helenavlasova39@gmail.com>
2021-01-19 17:20:06 +0300

    print numberOfSubheaderRows on the page for js

--- __StyleAndJavaScriptUtil.js
+++ __StyleAndJavaScriptUtil.js
@@ -106,6 +106,9 @@ class StyleAndJavaScriptUtil {
 
             var isDimensionsVisible = PageResults.isDimensionsMode(context);
             properties.push('isDimensionsTabVisible: ' + isDimensionsVisible);
+
+            var numberOfSubheaderRows = PageResults.getNumberOfSubHeaderRows(context);
+            properties.push('numberOfSubheaderRows: ' + numberOfSubheaderRows);
         }
 
         if (pageId === 'Categorical_') {
