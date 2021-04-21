1e89218f5b84c58b3bde4237e004050abb3c4336
ElenaV <helenavlasova39@gmail.com>
2021-04-21 11:20:30 +0300

    different way to determine bar chart values

--- __PageCulturalBehaviors.js
+++ __PageCulturalBehaviors.js
@@ -145,9 +145,9 @@ class PageCulturalBehaviors {
 
             bcCategories.HideData = true;
 
-            for (i = 0; i < n; i++) {
+            for (i = n; i > 0; i--) {
                 var chartValue: ChartComboValue = new ChartComboValue();
-                chartValue.Expression = 'cellv(col-' + (i + 1) + ', row)'; //'cellv(col-'+(n-i)+', row)';//
+                chartValue.Expression = 'cellv(col-' + (i) + ', row)'; //'cellv(col-'+(n-i)+', row)';//
                 chartValue.BaseColor = new ChartComboColorSet([barChartColors[i].color]);
                 chartValue.Name = TextAndParameterUtil.getTextTranslationByKey(context, barChartColors[i].label);
                 chartValue.CssClass = 'barchart__bar barchart__bar_type_distribution ' + barChartColors[i].type;
